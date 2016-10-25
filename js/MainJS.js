2a58b7b49edee33afe7b6d286dbfaeb7440c32df
Colin M St. Claire <colin.stclaire@wsu.edu>
2016-10-25 14:01:30 -0700

    streamlined a JS function, and have not pushed in a while

--- js/MainJS.js
+++ js/MainJS.js
@@ -2,15 +2,17 @@
  * Created by colinstclaire on 10/18/16.
  */
 
-function breweryDBGetBeer(beer) {
+function breweryDBGet(endpoint, parameter) {
+    var url = "http://api.brewerydb.com/v2/" + endpoint + "?key=3dad4576e0edfa7128dbb5831f6968b8&name=" + parameter + "";
     $.ajax({
         type: "GET",
-        url: "http://api.brewerydb.com/v2/beers?key=3dad4576e0edfa7128dbb5831f6968b8&name=" + beer +"",
-        //data: args,
+        url: url,
         contentType: "application/json; charset=urf-8",
+        crossDomain: true,
         dataType: "json",
         success: function (data) {
             // populate results
+            alert("SUCCESS");
             var myData = JSON.parse(data);
             $.each(myData, function(index, element) {
                 alert("index: " + index + ", element: " + element);
