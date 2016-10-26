c35b6e54192e8ad27e56e224ebeb3d2eea3a076b
Colin M St. Claire <colin.stclaire@wsu.edu>
2016-10-26 12:58:33 -0700

    Added more functionality to server.js

--- js/server.js
+++ js/server.js
@@ -8,3 +8,44 @@ var bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 app.set('port', 3000);
+
+app.get('/', function(req, res, next){
+    var userInput = processData(req);
+    request("https://api.brewerydb.com/v2/search?key=3dad4576e0edfa7128dbb5831f6968b8&q=" + userInput.qParams[0].value,
+        function (error, response, body) {
+            if (!error && response.statusCode === 200) {
+                console.log(body);
+                res.send(body);
+            }
+    });
+});
+
+app.use(function (req, res, next) {
+    // Website sending requests
+    res.setHeader('Access-Control-Allow-Origin', 'http://52.40.59.238');
+    // Request method that you are allowing (we are using GET)
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+    // Request header types that are allowed
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // The following is set to false since we won't be addressing cookies and sessions
+    res.setHeader('Access-Control-Allow-Credentials', false);
+    // Proceed to the next layer of middleware
+    next();
+});
+
+function processData(req) {
+    var context = {};
+    context.method = req.method; //method type saved
+    context.qParams = []; //query parameters
+    for(var p in req.query) {
+        context.qParams.push({
+            'name': p,
+            'value': req.query[p]
+        });
+    }
+    return context;
+}
+
+app.listen(app.get('port'), function(){
+    console.log('Express started on port' + app.get('port') + '; press Ctrl-C to terminate.');
+});
