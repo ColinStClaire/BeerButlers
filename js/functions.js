f044ccdca4b71e4d88f3ed53cb55a33b4b19aac0
Colin M St. Claire <colin.stclaire@wsu.edu>
2016-11-01 16:32:55 -0700

    ADRIENE: Started appending data to html elements

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,3 @@
-///
-
 /**
  * Created by colinstclaire on 10/31/16.
  */
@@ -11,7 +9,7 @@
  the results page.
  Modified: 11/01/16
  ---------------------------------------------*/
-function parseJSON(filePath) {
+/*function parseJSON(filePath) {
     var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
     var request = new XMLHttpRequest();
     request.open("GET", filePath, false);
@@ -24,8 +22,24 @@ function parseJSON(filePath) {
             console.log("ERROR");
         }
     }
-}
+}*/
 
-parseJSON("/data/stout.json")
+//parseJSON("/data/stout.json");
+
+/*---------------------------------------------
+ Developer: Colin St. Claire
+ Function Name: getJSON(filePath);
+ Description: The beginnings of the function that
+ will parse our JSON files and append HTML to
+ the results page.
+ Modified: 11/01/16
+ ---------------------------------------------*/
+function getJSON(filePath) {
+    $.getJSON(filePath, function (json) {
+        $("<img src='" + json.data[0].labels.medium + "'>").appendTo("#BeerHolder1");
+        $("<p>" + json.data[0].name + "</p>").appendTo("#BeerHolder1");
+
+    });
+}
 
 console.log("done in functions.js");
